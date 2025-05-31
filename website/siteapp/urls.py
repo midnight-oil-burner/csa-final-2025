619f4a083017a718e9e3d05a29a980c9efdb110b
midnight-oil-burner <aaronyan745@gmail.com>
2025-05-31 14:48:51 -0400

    finished unit 1

--- website/siteapp/urls.py
+++ website/siteapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:num>", views.unit, name="unit") 
+    path("<int:num>", views.unit, name="unit"), 
+    path("unit.css", views.check, name="check") # just for debugging fml
 ]
